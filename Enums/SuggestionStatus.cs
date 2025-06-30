1e148df5ca4ed128182075a7d0df4fbdc1ba5062
Andrej Grabovac <andrej.grabovac@gmail.com>
2025-06-30 23:15:52 +0200

    Added enums

---
+++ Enums/SuggestionStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeaHub.Enums
+{
+    public enum SuggestionStatus
+    {
+        [Description("Under review")]
+        UnderReview = 1,
+
+        [Description("Pending")]
+        Pending = 2,
+
+        [Description("Accepted")]
+        Accepted = 3,
+
+        [Description("Rejected")]
+        Rejected = 4
+    }
+}
