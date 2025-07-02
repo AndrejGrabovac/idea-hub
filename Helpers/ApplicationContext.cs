bc4b31d6502a0aa22a75ad2acf5cd5b6007b9d13
Andrej Grabovac <andrej.grabovac@gmail.com>
2025-07-02 23:40:35 +0200

    created helper folder with application context

---
+++ Helpers/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using IdeaHub.DTOs.User;
+using IdeaHub.Enums;
+using IdeaHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeaHub.Helpers
+{
+    public static class ApplicationContext
+    {
+        public static UserViewDto CurrentUser { get; internal set; }
+        public static bool IsLoggedIn => CurrentUser != null;
+        public static bool IsAdmin => CurrentUser?.Role == UserRole.Admin;
+        public static void ClearCurrentUser()
+        {
+            CurrentUser = null;
+        }
+    }
+}
